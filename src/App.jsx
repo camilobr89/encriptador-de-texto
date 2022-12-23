19c8c42c33ac0ae68d0e214800450747e4b6eb7b
camilobr89 <camilobr89@hotmail.com>
2022-12-23 12:35:13 -0500

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import reactLogo from './assets/Vector.png'
+import style from './App.css'
+
+function App() {
+
+
+  return (
+    <div className= {style.App}>
+      <header className= {style.AppHeader}>
+        <img src={reactLogo} className= {style.AppLogo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        </header>
+
+                            
+    </div>
+  )
+}
+
+export default App
